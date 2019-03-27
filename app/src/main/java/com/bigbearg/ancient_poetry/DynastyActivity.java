60e2f6f545e3ff097f7dbab94fcd119479cbd38a
admin <you@example.com>
2019-03-27 15:46:43 +0800

    bar

--- app/src/main/java/com/bigbearg/ancient_poetry/DynastyActivity.java
+++ app/src/main/java/com/bigbearg/ancient_poetry/DynastyActivity.java
@@ -5,15 +5,28 @@ import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.util.TypedValue;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.bigbearg.ancient_poetry.Bean.DynastyBean;
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.charts.LineChart;
 import com.github.mikephil.charting.charts.PieChart;
 import com.github.mikephil.charting.components.AxisBase;
 import com.github.mikephil.charting.components.Description;
 import com.github.mikephil.charting.components.Legend;
 import com.github.mikephil.charting.components.LegendEntry;
 import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
 import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
 import com.github.mikephil.charting.data.PieData;
 import com.github.mikephil.charting.data.PieDataSet;
 import com.github.mikephil.charting.data.PieEntry;
@@ -22,6 +35,7 @@ import com.github.mikephil.charting.formatter.IValueFormatter;
 import com.github.mikephil.charting.utils.ColorTemplate;
 import com.github.mikephil.charting.utils.ViewPortHandler;
 
+import java.io.ByteArrayInputStream;
 import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.List;
@@ -30,13 +44,20 @@ public class DynastyActivity extends AppCompatActivity {
 
     private PieChart mDynastyChart;
     private List<DynastyBean> dynastyBeans;
+    private LinearLayout mContentLayout;
+    private BarChart mBarChart;
+    private LineChart mLineChart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dynasty);
+        mContentLayout = findViewById(R.id.chart_content_layout);
         mDynastyChart = findViewById(R.id.dynasty_pie_chart);
+        mBarChart = findViewById(R.id.dynasty_bar_chart);
+//        mLineChart = findViewById(R.id.dynasty_line_chart);
         getData();
+        initView();
     }
 
     private void getData() {
@@ -49,6 +70,9 @@ public class DynastyActivity extends AppCompatActivity {
         dynastyBeans.add(new DynastyBean("清朝",7));
 
         initView();
+        initBarChart();
+//        initLineChart();
+
     }
 
     private void initView(){
@@ -106,6 +130,94 @@ public class DynastyActivity extends AppCompatActivity {
         mDynastyChart.setData(data);
         mDynastyChart.invalidate();
     }
+
+    private void initBarChart(){
+        List<BarEntry> barEntries = new ArrayList<>();
+        try{
+            for(int i=0; i<dynastyBeans.size(); i++){
+                DynastyBean bean  = dynastyBeans.get(i);
+                barEntries.add(new BarEntry(i+1,bean.getCount()));
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        Legend l = mBarChart.getLegend();//获取图例对象
+
+
+
+//        l.setEnabled(false);//禁用图例,直接禁用X轴会显示不全
+
+
+ l.setForm(Legend.LegendForm.NONE);
+// l.setTextColor(Color.parseColor("#ffffff"));
+
+
+        XAxis xAxis = mBarChart.getXAxis();
+        xAxis.setTextSize(13);
+        xAxis.setDrawAxisLine(false);
+        xAxis.setDrawGridLines(false);
+        xAxis.setLabelCount(dynastyBeans.size());
+        xAxis.setValueFormatter(new IAxisValueFormatter() {
+            @Override
+            public String getFormattedValue(float v, AxisBase axisBase) {
+                return dynastyBeans.get((int)v-1).getDynastyName();
+            }
+        });
+
+
+        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+        mBarChart.getAxisRight().setEnabled(false);
+        mBarChart.getAxisLeft().setTextSize(13);
+        mBarChart.setTouchEnabled(false);
+        mBarChart.setScaleEnabled(false);
+        mBarChart.setDragEnabled(false);
+        BarDataSet barDataSet = new BarDataSet(barEntries," ");
+        barDataSet.setValueTextSize(12);
+        barDataSet.setValueFormatter(new IValueFormatter() {
+            @Override
+            public String getFormattedValue(float v, Entry entry, int i, ViewPortHandler viewPortHandler) {
+                return (int)v+"次";
+            }
+        });
+        barDataSet.setColors(new int[]{Color.parseColor("#FFDAB9"),
+                Color.parseColor("#C6E2FF"),
+                Color.parseColor("#E0EEEE"),
+                Color.parseColor("#C1FFC1"),
+                Color.parseColor("#C71585")});
+
+        BarData barData = new BarData(barDataSet);
+        Description description = new Description();
+        description.setText("");
+        mBarChart.setDescription(description);
+        mBarChart.setData(barData);
+        mBarChart.invalidate();
+    }
+
+//    private void initLineChart(){
+//        List<Entry> entries = new ArrayList<>();
+//        try{
+//            for(int i=0; i<dynastyBeans.size(); i++){
+//                DynastyBean bean  = dynastyBeans.get(i);
+//                entries.add(new Entry(i+1,bean.getCount()));
+//            }
+//        }catch (Exception e){
+//            e.printStackTrace();
+//        }
+//        LineDataSet lineDataSet = new LineDataSet(entries," ");
+//        LineData lineData = new LineData(lineDataSet);
+//        Description description = new Description();
+//        lineDataSet.setColors(new int[]{Color.parseColor("#FFDAB9"),
+//                Color.parseColor("#C6E2FF"),
+//                Color.parseColor("#E0EEEE"),
+//                Color.parseColor("#C1FFC1"),
+//                Color.parseColor("#C71585")});
+//        description.setText("");
+//        mLineChart.setDescription(description);
+//        mLineChart.setData(lineData);
+//        mLineChart.invalidate();
+//    }
+
+
 
 
 }
